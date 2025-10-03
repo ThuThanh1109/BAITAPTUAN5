fcbf4a5f5e3a45c52a5247570dc1d05549efc020
Phạm Dương Thúy Hằng <hang33423@gmail.com>
2025-10-03 22:06:33 +0700

    Add files via upload

---
+++ Sach.java
@@ -0,0 +1,73 @@
+package Sach;
+
+public class Sach {
+
+    private String maSach;
+    private String tieuDe;
+    private String tacGia;
+    private int namXuatBan;
+    private int soLuong;
+    
+    // Phương thức hiển thị thông tin sách
+    public void hienThiThongTin() {
+        System.out.println("Ma sach: " + maSach);
+        System.out.println("Tieu de: " + tieuDe);
+        System.out.println("Tac gia: " + tacGia);
+        System.out.println("Nam xuat ban: " + namXuatBan);
+        System.out.println("So luong: " + soLuong);
+    }
+    
+    // Constructor mặc định
+     public Sach() {
+    }
+     
+    // Constructor đầy đủ tham số
+    public Sach(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong) {
+        this.maSach = maSach;
+        this.tieuDe = tieuDe;
+        this.tacGia = tacGia;
+        this.namXuatBan = namXuatBan;
+        this.soLuong = soLuong;
+    }
+    
+    // Getter và Setter cho từng thuộc tính
+    public String getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
+
+    public String getTieuDe() {
+        return tieuDe;
+    }
+
+    public void setTieuDe(String tieuDe) {
+        this.tieuDe = tieuDe;
+    }
+
+    public String getTacGia() {
+        return tacGia;
+    }
+
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
+
+    public int getNamXuatBan() {
+        return namXuatBan;
+    }
+
+    public void setNamXuatBan(int namXuatBan) {
+        this.namXuatBan = namXuatBan;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+}
